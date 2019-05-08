b2ae57e84bd742d3208cec960e6f00ddfaa96a97
Khadim Mbow <kmbow@na.edu>
2019-05-08 14:39:28 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,62 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+var path = require('path');
+const MongoClient = require('mongodb').MongoClient;
+
+var PORT = process.env.PORT || 3000;
+
+//connect to MongoDB
+mongoose.connect('mongodb://admin:Salamatane_009@ds229295.mlab.com:29295/heroku_b3z28c88');
+var db = mongoose.connection;
+
+//handle mongo error
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  // we're connected!
+});
+
+//use sessions for tracking logins
+app.use(session({
+  secret: 'work hard',
+  resave: true,
+  saveUninitialized: false,
+  store: new MongoStore({
+    mongooseConnection: db
+  })
+}));
+
+// parse incoming requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+// serve static files from template
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+// include routes
+var routes = require('./routes/router');
+app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('File Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+// define as the last app.use callback
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.send(err.message);
+});
+
+
+// listen on port 3000
+app.listen(PORT, function () {
+  console.log('Server is running on port '+ PORT);
+});
